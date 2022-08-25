1ff09d2c5fcfd4d0f0b8fc2b661cc0ce6ad9061a
justinbanaga <justinbanaga.195@gmail.com>
2022-08-25 18:23:43 +0800

    refactor: :art: moved list variable from global scope to app component to be passed as props to list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   const handleChange = (event) => {
     console.log(event.target.value);
   };
@@ -31,14 +31,14 @@ const App = () => {
 
       <hr />
 
-      <List />
+      <List list={stories} />
     </div>
   );
 };
 
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => {
+    {props.list.map((item) => {
       return (
         <li key={item.objectId}>
           <a href={item.url}>{item.title}</a>
